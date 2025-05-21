cafcc1830dbb9f289fa5f08c343c75c5421cf1a5
Rafael Gomes <gorraffa@hotmail.com>
2025-05-20 22:43:26 -0300

    search seller

---
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleNameDTO.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dsmeta.dto;
+
+public class SaleNameDTO {
+
+    private Long id;
+    private String name;
+}
--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,16 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.entities.Seller;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("Select obj FROM Seller obj WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%',:name, '%'))")
+    Page<Seller> searchByName(String name, Pageable pageable);
 
 }
